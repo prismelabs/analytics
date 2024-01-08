b6fa21e1002900840190d4944046f8856c1fd871
Alexandre Negrel <alexandre@negrel.dev>
2024-01-08 18:41:21 +0100

    update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
+	"github.com/labstack/echo/v4"
+	"github.com/prismelabs/prismeanalytics/internal/config"
 	"github.com/prismelabs/prismeanalytics/internal/log"
+	"github.com/prismelabs/prismeanalytics/internal/middlewares"
 )
 
 func main() {
-	logger := log.NewLogger("app", os.Stderr, false)
+	cfg := config.ServerFromEnv()
+
+	logger := log.NewLogger("app", os.Stderr, cfg.Debug)
+	accessLogger := log.NewLogger("access_log", os.Stderr, cfg.Debug)
 	log.TestLoggers(logger)
+
+	_ = accessLogger
+
+	e := echo.New()
+	if cfg.TrustProxy {
+		e.IPExtractor = echo.ExtractIPFromXFFHeader()
+	} else {
+		e.IPExtractor = echo.ExtractIPDirect()
+	}
+	e.HideBanner = true
+	e.HidePort = true
+
+	e.Use(middlewares.RequestId(cfg))
+	e.Use(middlewares.AccessLog(accessLogger))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	socket := "0.0.0.0:8000"
+	logger.Info().Msgf("start listening for incoming requests on http://%v", socket)
+	logger.Panic().Err(e.Start(socket)).Send()
 }
