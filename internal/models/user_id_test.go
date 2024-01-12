df92a5f804b52ac1840d0691c49deb29bd884124
Alexandre Negrel <alexandre@negrel.dev>
2024-01-12 11:51:37 +0100

    add models package

---
+++ internal/models/user_id_test.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseUserId(t *testing.T) {
+	t.Run("NotAUuid", func(t *testing.T) {
+		uid, err := ParseUserId("")
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUserIdIsNotAUuidV4)
+		require.Equal(t, UserId{}, uid)
+	})
+
+	t.Run("UuidV6", func(t *testing.T) {
+		uid, err := ParseUserId(uuid.Must(uuid.NewV6()).String())
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUserIdIsNotAUuidV4)
+		require.Equal(t, UserId{}, uid)
+	})
+
+	t.Run("UuidV4", func(t *testing.T) {
+		rawUuid := uuid.New().String()
+		uid, err := ParseUserId(rawUuid)
+		require.NoError(t, err)
+		require.Equal(t, rawUuid, uid.String())
+	})
+}
---
+++ internal/models/user_name_test.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewUserName(t *testing.T) {
+	t.Run("TooShort", func(t *testing.T) {
+		un, err := NewUserName("🏳️‍🌈")
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUserNameTooShort)
+		require.Equal(t, UserName{}, un)
+	})
+
+	t.Run("TooLong", func(t *testing.T) {
+		un, err := NewUserName(strings.Repeat("Foo ", 16)) // length of 64
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUserNameTooLong)
+		require.Equal(t, UserName{}, un)
+	})
+
+	t.Run("Valid", func(t *testing.T) {
+		un, err := NewUserName("Foo")
+		require.NoError(t, err)
+		require.Equal(t, "Foo", un.String())
+	})
+}
