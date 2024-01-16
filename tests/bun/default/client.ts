715de656a42a8b91157191eaea92c7f7e364ae8b
Alexandre Negrel <alexandre@negrel.dev>
2024-01-16 13:46:25 +0100

    test: add e2e sign in tests

--- tests/bun/default/client.ts
+++ tests/bun/default/client.ts
@@ -32,3 +32,30 @@ export async function signUp (body: SignUpRequest): Promise<SessionCookie> {
 
   return `${cookie.name}=${cookie.value}`
 }
+
+export interface SignInRequest {
+  email: string
+  password: string
+}
+
+export async function signIn (body: SignInRequest): Promise<SessionCookie> {
+  const response = await postForm('/sign_in', body)
+  expect(response.status).toBe(302)
+  expect(response.headers.getSetCookie()).toHaveLength(1)
+
+  const cookie = parseSetCookie(response.headers.getSetCookie()[0])
+  expect({ ...cookie }).toMatchObject({
+    name: 'prisme_session_id',
+    value: expect.stringMatching(UUID_V4_REGEX),
+    attributes: {
+      'max-age': 86400,
+      path: '/',
+      HttpOnly: true,
+      secure: true,
+      SameSite: 'Strict'
+    }
+  })
+  expect(response.headers.get('Location')).toBe('/')
+
+  return `${cookie.name}=${cookie.value}`
+}
---
+++ tests/bun/default/sign_in.test.ts
@@ -0,0 +1,92 @@
+import { expect, test } from 'bun:test'
+import { faker } from '@faker-js/faker'
+import { JSDOM } from 'jsdom'
+
+import { PRISME_URL, postForm } from './utils'
+import { signIn, signUp } from './client'
+
+const seed = new Date().getTime()
+console.log('faker seed', seed)
+faker.seed(seed)
+
+test('sign in page contains login form', async () => {
+  const response = await fetch(PRISME_URL + '/sign_in')
+  expect(response.status).toBe(200)
+  const html = await response.text()
+  const { window: { document } } = new JSDOM(html)
+
+  expect(document.querySelector('form[method=POST]')).not.toBeNil()
+  expect(document.querySelector('input[type=email]#email')).not.toBeNil()
+  expect(document.querySelector('input[type=password]#password')).not.toBeNil()
+})
+
+test('sign in without required email field', async () => {
+  const response = await postForm('/sign_in', {
+    // email: faker.internet.email(),
+    password: faker.internet.password()
+  })
+  expect(response.status).toBe(400)
+
+  const body = await response.text()
+  expect(body).toContain('email invalid: mail: no address')
+})
+
+test('sign in without required password field', async () => {
+  const response = await postForm('/sign_in', {
+    email: faker.internet.email()
+    // password: faker.internet.password()
+  })
+  expect(response.status).toBe(401)
+
+  const body = await response.text()
+  expect(body).toContain('login or password incorrect')
+})
+
+test('sign in with invalid email (no user registered with that email)', async () => {
+  const response = await postForm('/sign_in', {
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  })
+  expect(response.status).toBe(401)
+})
+
+test('sign in with invalid password', async () => {
+  const email = faker.internet.email()
+  const password = faker.internet.password()
+
+  await signUp({ name: faker.internet.userName(), email, password })
+
+  const response = await postForm('/sign_in', {
+    email,
+    password: faker.internet.password()
+  })
+  expect(response.status).toBe(401)
+
+  const body = await response.text()
+  expect(body).toContain('login or password incorrect')
+})
+
+test('sign in with valid credentials', async () => {
+  const email = faker.internet.email()
+  const password = faker.internet.password()
+
+  await signUp({ name: faker.internet.userName(), email, password })
+
+  const cookieHeader = await signIn({
+    email,
+    password
+  })
+
+  {
+    const response = await fetch(PRISME_URL, {
+      headers: {
+        Cookie: cookieHeader
+      },
+      redirect: 'error'
+    })
+    expect(response.status).toBe(200)
+
+    const body = await response.text()
+    expect(body).toContain('<title>Home - Prisme Analytics</title>')
+  }
+})
