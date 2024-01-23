15e33f628c733b8c89e0bf31bd090d163d13b804
Alexandre Negrel <alexandre@negrel.dev>
2024-01-23 18:54:59 +0100

    add minimal pageview js script

---
+++ internal/embedded/static/m.js
@@ -0,0 +1,13 @@
+(function () {
+  var ps = document.currentScript.dataset.prismeScheme || "https";
+  var pd = document.currentScript.dataset.prismeDomain || "prismeanalytics.com";
+  var s = location.protocol;
+  var d = document.currentScript.dataset.domain || location.host;
+  fetch(ps.concat("://", pd, "/api/v1/events/pageviews"), {
+    method: "POST",
+    headers: {
+      "X-Prisme-Referrer": s.concat('//', d, location.pathname)
+    },
+    referrerPolicy: "no-referrer-when-downgrade"
+  });
+})();
