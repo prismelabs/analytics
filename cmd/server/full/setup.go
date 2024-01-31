91fb75ad6963435614fa4a2ba58a246d3387d161
Alexandre Negrel <alexandre@negrel.dev>
2024-01-31 11:54:30 +0100

    wait for healthy grafana before starting server

--- cmd/server/full/setup.go
+++ cmd/server/full/setup.go
@@ -5,12 +5,14 @@ import (
 	"time"
 
 	"github.com/prismelabs/prismeanalytics/cmd/server/wired"
+	grafanaCli "github.com/prismelabs/prismeanalytics/internal/grafana"
 	"github.com/prismelabs/prismeanalytics/internal/log"
 	"github.com/prismelabs/prismeanalytics/internal/services/grafana"
 )
 
 // ProvideSetup is a wire provider that performs setup of full server.
-func ProvideSetup(logger log.Logger, grafanaService grafana.Service) wired.Setup {
+func ProvideSetup(logger log.Logger, cli grafanaCli.Client, grafanaService grafana.Service) wired.Setup {
+	grafanaCli.WaitHealthy(logger, cli, 5)
 	logger.Info().Msg("setting up grafana datasource and dashboards...")
 	{
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
