b67e9b16ce2d38052409374d33a57c1b46d9a817
Alexandre Negrel <alexandre@negrel.dev>
2024-02-05 10:14:51 +0100

    update client script to handle PWA history back/forward

--- internal/embedded/static/m.js
+++ internal/embedded/static/m.js
@@ -1,19 +1,23 @@
 (function () {
-	var ps = document.currentScript?.dataset?.prismeScheme || "https";
-	var pd = document.currentScript?.dataset?.prismeDomain || "prismeanalytics.com";
-	var s = location.protocol;
-	var d = document.currentScript?.dataset?.domain || location.host;
-	var referrer = document.referrer
+  var ps = document.currentScript.dataset.prismeScheme || "https";
+  var pd = document.currentScript.dataset.prismeDomain || "prismeanalytics.com";
+  var s = location.protocol;
+  var d = document.currentScript.dataset.domain || location.host;
+  var r = document.referrer;
 
   function pageview() {
+    var u = s.concat('//', d, location.pathname)
+
     fetch(ps.concat("://", pd, "/api/v1/events/pageviews"), {
       method: "POST",
       headers: {
-        "X-Prisme-Referrer": s.concat('//', d, location.pathname),
-        "X-Prisme-Document-Referrer": referrer
+        "X-Prisme-Referrer": u,
+        "X-Prisme-Document-Referrer": r,
       },
       referrerPolicy: "no-referrer-when-downgrade"
     });
+
+    r = u
   }
 
   pageview();
@@ -21,11 +25,7 @@
   if (window.history) {
     var pushState = window.history.pushState;
     window.history.pushState = function() {
-      if (location.pathname === referrer) {
-        return;
-      }
-      referrer = new URL(location.pathname, location.protocol.concat("//", d))
-      pushState.apply(this, arguments);
+      pushState.apply(window.history, arguments);
       pageview();
     }
     window.addEventListener('popstate', pageview)
