cffed42c6cbf8f05f9c71dea7787a7d801e1779d
Alexandre Negrel <alexandre@negrel.dev>
2024-03-19 12:10:42 +0100

    add custom events tests

---
+++ tests/bun/events/events_customs.test.ts
@@ -0,0 +1,209 @@
+import { expect, test } from 'bun:test'
+import { faker } from '@faker-js/faker'
+
+import { createClient } from '@clickhouse/client-web'
+import { PRISME_CUSTOM_EVENTS_URL, TIMESTAMP_REGEX } from '../const'
+
+const seed = new Date().getTime()
+console.log('faker seed', seed)
+faker.seed(seed)
+
+test('GET request instead of POST request', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'GET',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'http://mywebsite.localhost/foo',
+      'Content-Type': 'application/json'
+    }
+    // body: JSON.stringify({}) // GET request can't have body.
+  })
+  expect(response.status).toBe(405)
+})
+
+test('invalid URL in X-Prisme-Referrer header', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'not an url',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+  })
+  expect(response.status).toBe(400)
+})
+
+test('non registered domain in Origin header is rejected', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'https://example.com',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'https://example.com/foo?bar=baz#qux',
+      'Content-Type': 'application/json',
+      body: JSON.stringify({})
+    }
+  })
+  expect(response.status).toBe(400)
+})
+
+test('content type different than application/json is rejected', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'https://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'https://mywebsite.localhost/foo?bar=baz#qux',
+      'Content-Type': 'text/plain'
+    }
+  })
+  expect(response.status).toBe(400)
+})
+
+test('valid custom event request without body', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'http://mywebsite.localhost/index.html',
+      'Content-Type': 'application/json'
+    }
+  })
+  expect(response.status).toBe(200)
+
+  const data = await getLatestCustomEvent()
+
+  expect(data).toMatchObject({
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: 'mywebsite.localhost',
+    path: '/index.html',
+    name: 'foo',
+    properties: JSON.stringify({})
+  })
+})
+
+test('valid custom event with no properties', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'http://mywebsite.localhost/index.html',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+  })
+  expect(response.status).toBe(200)
+
+  const data = await getLatestCustomEvent()
+
+  expect(data).toMatchObject({
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: 'mywebsite.localhost',
+    path: '/index.html',
+    name: 'foo',
+    properties: JSON.stringify({})
+  })
+})
+
+test('valid custom event with few properties', async () => {
+  const props = JSON.stringify({
+    x: Math.round(Math.random() * 100),
+    y: Math.round(Math.random() * 100)
+  })
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'http://mywebsite.localhost/index.html',
+      'Content-Type': 'application/json'
+    },
+    body: props
+  })
+  expect(response.status).toBe(200)
+
+  const data = await getLatestCustomEvent()
+
+  expect(data).toMatchObject({
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: 'mywebsite.localhost',
+    path: '/index.html',
+    name: 'foo',
+    properties: props
+  })
+})
+
+test('valid custom event with lot of properties', async () => {
+  const props: Record<number, number> = {}
+  for (let i = 0; i < 1024; i++) {
+    props[i] = Math.round(Math.random() * 100)
+  }
+  const serializedProps = JSON.stringify(props)
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      'X-Prisme-Referrer': 'http://mywebsite.localhost/index.html',
+      'Content-Type': 'application/json'
+    },
+    body: serializedProps
+  })
+  expect(response.status).toBe(200)
+
+  const data = await getLatestCustomEvent()
+
+  expect(data).toMatchObject({
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: 'mywebsite.localhost',
+    path: '/index.html',
+    name: 'foo',
+    properties: serializedProps
+  })
+})
+
+test('valid custom event without X-Prisme-Referrer', async () => {
+  const response = await fetch(PRISME_CUSTOM_EVENTS_URL + '/foo', {
+    method: 'POST',
+    headers: {
+      Origin: 'http://mywebsite.localhost',
+      'X-Forwarded-For': faker.internet.ip(),
+      Referer: 'http://mywebsite.localhost/index.html',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+  })
+  expect(response.status).toBe(200)
+
+  const data = await getLatestCustomEvent()
+
+  expect(data).toMatchObject({
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: 'mywebsite.localhost',
+    path: '/index.html',
+    name: 'foo',
+    properties: JSON.stringify({})
+  })
+})
+
+async function getLatestCustomEvent (): Promise<any> {
+  // Wait for clickhouse to ingest batch.
+  Bun.sleepSync(1000)
+
+  const client = createClient({
+    host: 'http://clickhouse.localhost:8123',
+    username: 'clickhouse',
+    password: 'password',
+    database: 'prisme'
+  })
+
+  const rows = await client.query({
+    query: 'SELECT * FROM prisme.events_custom ORDER BY timestamp DESC LIMIT 1;'
+  })
+  return rows.json().then((r: any) => r.data[0])
+}
