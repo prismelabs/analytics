808ee20f82c22935abb7276a461f13e702970892
Alexandre Negrel <alexandre@negrel.dev>
2024-05-03 21:00:21 +0200

    add e2e tests for entry pages and exit pages tables

--- tests/bun/const.ts
+++ tests/bun/const.ts
@@ -11,3 +11,4 @@ export const TIMESTAMP_REGEX = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
 export const UUID_V4_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 export const COUNTRY_CODE_REGEX = /^[A-Z]{2}$/
 export const PRISME_VISITOR_ID_REGEX = /prisme_.+/
+export const SESSION_ID_REGEX = /[0-9]+/
---
+++ tests/bun/events/session.test.ts
@@ -0,0 +1,162 @@
+import { expect, test } from 'bun:test'
+import { faker } from '@faker-js/faker'
+
+import { createClient } from '@clickhouse/client-web'
+import { COUNTRY_CODE_REGEX, PRISME_PAGEVIEWS_URL, PRISME_VISITOR_ID_REGEX, SESSION_ID_REGEX, TIMESTAMP_REGEX } from '../const'
+
+test('multiple page view session', async () => {
+  const ipAddr = faker.internet.ip()
+
+  let response = await fetch(PRISME_PAGEVIEWS_URL, {
+    method: 'POST',
+    headers: {
+      Origin: 'http://foo.mywebsite.localhost',
+      'X-Forwarded-For': ipAddr,
+      'X-Prisme-Referrer': 'http://foo.mywebsite.localhost/page1'
+    }
+  })
+  expect(response.status).toBe(200)
+
+  // Fetch latest exit and entry page.
+  let exitPage = await getLatestExitPage()
+  let entryPage = await getLatestEntryPage()
+
+  // Session ID must not match.
+  if (exitPage !== undefined) { expect(exitPage.session_id).not.toBe(entryPage.session_id) }
+
+  // Check entry page.
+  const entryPageMatcher = {
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: 'foo.mywebsite.localhost',
+    path: '/page1',
+    operating_system: 'Other',
+    browser_family: 'Other',
+    device: 'Other',
+    referrer_domain: 'direct',
+    country_code: expect.stringMatching(COUNTRY_CODE_REGEX),
+    visitor_id: expect.stringMatching(PRISME_VISITOR_ID_REGEX),
+    session_id: expect.stringMatching(SESSION_ID_REGEX)
+  }
+  expect(entryPage).toMatchObject(entryPageMatcher)
+
+  // View another page.
+  response = await fetch(PRISME_PAGEVIEWS_URL, {
+    method: 'POST',
+    headers: {
+      Origin: 'http://foo.mywebsite.localhost',
+      'X-Forwarded-For': ipAddr,
+      'X-Prisme-Document-Referrer': 'http://foo.mywebsite.localhost/page1',
+      'X-Prisme-Referrer': 'http://foo.mywebsite.localhost/page2'
+    }
+  })
+
+  exitPage = await getLatestExitPage()
+  entryPage = await getLatestEntryPage()
+
+  // Exit page was added.
+  expect(exitPage).toMatchObject({
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: entryPage.domain,
+    path: '/page2',
+    operating_system: entryPage.operating_system,
+    browser_family: entryPage.browser_family,
+    device: entryPage.device,
+    referrer_domain: entryPage.domain,
+    country_code: entryPage.country_code,
+    visitor_id: entryPage.visitor_id,
+    session_id: entryPage.session_id,
+    entry_timestamp: entryPage.timestamp
+  })
+
+  // Entry page remains the same.
+  expect(entryPage).toMatchObject(entryPageMatcher)
+
+  // View a third page.
+  response = await fetch(PRISME_PAGEVIEWS_URL, {
+    method: 'POST',
+    headers: {
+      Origin: 'http://foo.mywebsite.localhost',
+      'X-Forwarded-For': ipAddr,
+      'X-Prisme-Document-Referrer': 'http://foo.mywebsite.localhost/page2',
+      'X-Prisme-Referrer': 'http://foo.mywebsite.localhost/page3'
+    }
+  })
+
+  exitPage = await getLatestExitPage()
+  entryPage = await getLatestEntryPage()
+
+  // New exit page replace /page2.
+  const exitPageMatcher = {
+    timestamp: expect.stringMatching(TIMESTAMP_REGEX),
+    domain: entryPage.domain,
+    path: '/page3',
+    operating_system: entryPage.operating_system,
+    browser_family: entryPage.browser_family,
+    device: entryPage.device,
+    referrer_domain: entryPage.domain,
+    country_code: entryPage.country_code,
+    visitor_id: entryPage.visitor_id,
+    session_id: entryPage.session_id,
+    entry_timestamp: entryPage.timestamp
+  }
+  expect(exitPage).toMatchObject(exitPageMatcher)
+
+  // Entry page remains the same.
+  expect(entryPage).toMatchObject(entryPageMatcher)
+
+  // Start a new session!
+  response = await fetch(PRISME_PAGEVIEWS_URL, {
+    method: 'POST',
+    headers: {
+      Origin: 'http://foo.mywebsite.localhost',
+      'X-Forwarded-For': ipAddr,
+      'X-Prisme-Referrer': 'http://foo.mywebsite.localhost/page4'
+      // No document referrer.
+    }
+  })
+  expect(response.status).toBe(200)
+
+  exitPage = await getLatestExitPage()
+  entryPage = await getLatestEntryPage()
+
+  // Exit page remains unchanged.
+  expect(exitPage).toMatchObject(exitPageMatcher)
+
+  // New entry page.
+  entryPageMatcher.path = '/page4'
+  expect(entryPage).toMatchObject(entryPageMatcher)
+}, { timeout: 30_000 })
+
+async function getLatestEntryPage (): Promise<any> {
+  // Wait for clickhouse to ingest batch.
+  Bun.sleepSync(1000)
+
+  const client = createClient({
+    host: 'http://clickhouse.localhost:8123',
+    username: 'clickhouse',
+    password: 'password',
+    database: 'prisme'
+  })
+
+  const rows = await client.query({
+    query: 'SELECT * FROM prisme.entry_pages ORDER BY timestamp DESC LIMIT 1;'
+  })
+  return await rows.json().then((r: any) => r.data[0])
+}
+
+async function getLatestExitPage (): Promise<any> {
+  // Wait for clickhouse to ingest batch.
+  Bun.sleepSync(1000)
+
+  const client = createClient({
+    host: 'http://clickhouse.localhost:8123',
+    username: 'clickhouse',
+    password: 'password',
+    database: 'prisme'
+  })
+
+  const rows = await client.query({
+    query: 'SELECT * FROM prisme.exit_pages_no_bounce FINAL ORDER BY timestamp DESC LIMIT 1;'
+  })
+  return await rows.json().then((r: any) => r.data[0])
+}
