a7b876dd457979c2aa1df4c1e8afd7346ef1c821
Alexandre Negrel <alexandre@negrel.dev>
2024-06-03 22:48:00 +0200

    remove deprecated unstable prisme tables

---
+++ pkg/embedded/ch_migrations/20240515195824_drop_entry_exit_pages_tables.up.sql
@@ -0,0 +1,8 @@
+DROP TABLE entry_pages;
+DROP TABLE entry_pages_mv;
+DROP TABLE entry_pages_datetime;
+DROP TABLE entry_pages_datetimes_mv;
+
+DROP TABLE exit_pages_mv;
+DROP TABLE exit_pages_no_bounce;
+
