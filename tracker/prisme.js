b1114d2aa0a63f9cc489d3777b120e5361690cb6
Alexandre Negrel <alexandre@negrel.dev>
2024-06-12 16:11:48 +0200

    add path and manual options to tracking script

--- tracker/prisme.js
+++ tracker/prisme.js
@@ -5,49 +5,74 @@
   var prismeUrl = currentScriptDataset.prismeUrl || currentScriptUrl.origin
   var scheme = location.protocol;
   var domain = currentScriptDataset.domain || location.host;
+  var path = currentScriptDataset.path || location.pathname;
+  var manual = !!currentScriptDataset.manual || false
   var referrer = document.referrer.replace(location.host, domain);
-  var pageUrl = function() { return location.toString().replace(location.host, domain) }
 
-  function pageview() {
+  // For better minification.
+  var referrerPolicy ="no-referrer-when-downgrade"
+  var method = "POST"
+  var accessControlMaxAgeHeader = "Access-Control-Max-Age"
+  var accessControlMaxAge = 3600
+  var prismeReferrerHeader = "X-Prisme-Referrer"
+
+  function defaultOptions(options) {
+    if (!options) options = {}
+    if (!("url" in options)) {
+      if (!manual)
+        options.url = location.origin.replace(location.host, domain) + path + location.search
+      else options.url = location.toString().replace(location.host, domain)
+    }
+
+    return options
+  }
+
+  function pageview(options) {
+    options = defaultOptions(options)
+
     fetch(prismeUrl.concat("/api/v1/events/pageviews"), {
       method: "POST",
       headers: {
-        "Access-Control-Max-Age": 3600,
-        "X-Prisme-Referrer": pageUrl(),
+        [accessControlMaxAgeHeader]: accessControlMaxAge,
+        [prismeReferrerHeader]: options.url.toString(),
         "X-Prisme-Document-Referrer": referrer,
       },
       keepalive: true,
-      referrerPolicy: "no-referrer-when-downgrade",
+      referrerPolicy: referrerPolicy
     });
 
-    referrer = pageUrl()
+    referrer = options.url.toString()
   }
 
-  window.prisme = {
-    trigger: function(eventName, properties) {
+  window.prisme ={
+    pageview,
+    trigger(eventName, properties, options) {
+      options = defaultOptions(options)
+
       fetch(prismeUrl.concat("/api/v1/events/custom/", eventName), {
         method: "POST",
         headers: {
-          "Access-Control-Max-Age": 3600,
-          "X-Prisme-Referrer": pageUrl(),
-          "X-Prisme-Document-Referrer": referrer,
+          [accessControlMaxAgeHeader]: accessControlMaxAge,
+          [prismeReferrerHeader]: options.url.toString(),
           "Content-Type": "application/json",
         },
         keepalive: true,
-        referrerPolicy: "no-referrer-when-downgrade",
+        referrerPolicy: referrerPolicy,
         body: JSON.stringify(properties)
       });
     }
   }
 
-  pageview();
+  if (!manual) {
+    pageview();
 
-  if (window.history) {
-    var pushState = window.history.pushState;
-    window.history.pushState = function() {
-      pushState.apply(window.history, arguments);
-      pageview();
+    if (window.history) {
+      var pushState = window.history.pushState;
+      window.history.pushState = function() {
+        pushState.apply(window.history, arguments);
+        pageview();
+      }
+      window.addEventListener('popstate', pageview)
     }
-    window.addEventListener('popstate', pageview)
   }
 })();
