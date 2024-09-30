d7b86d1cc094c9eb2a5381a27450b84c28eb77f4
Alexandre Negrel <alexandre@negrel.dev>
2024-09-30 13:15:29 +0200

    update go deps

--- flake.nix
+++ flake.nix
@@ -1,82 +1,78 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     let
       outputsWithoutSystem = { };
-      outputsWithSystem = flake-utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
+      outputsWithSystem = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          # lib = pkgs.lib;
+        in {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                go
+                mockgen # Go mock generator
+                gopls # Go LSP
+                golangci-lint # Go linter
+                wire # Go dependency injection
+                go-migrate # Go SQL migration
+                bunyan-rs # Bunyan format pretty print
+                entr # FS watcher
+                bun # Bun JS runtime
+                minify # JS minifier
+                clickhouse # clickhouse client
+                hyperfine # binary benchmarks
+              ];
             };
-            # lib = pkgs.lib;
-          in
-          {
-            devShells = {
-              default = pkgs.mkShell {
-                buildInputs = with pkgs; [
-                  go
-                  mockgen # Go mock generator
-                  gopls # Go LSP
-                  golangci-lint # Go linter
-                  wire # Go dependency injection
-                  go-migrate # Go SQL migration
-                  bunyan-rs # Bunyan format pretty print
-                  entr # FS watcher
-                  bun # Bun JS runtime
-                  minify # JS minifier
-                  clickhouse # clickhouse client
-                  hyperfine # binary benchmarks
-                ];
-              };
+          };
+          packages = {
+            default = pkgs.buildGoModule {
+              pname = "prisme";
+              version = "0.16.0";
+              vendorHash =
+                "sha256-F3ZUU+hixLOOZrDjfHOuxHnopzIdc/9qxR6rMeUbt/0=";
+
+              src = ./.;
+              # Skip go test.
+              doCheck = false;
+
+              postBuild = ''
+                mv "$GOPATH/bin/server" "$GOPATH/bin/prisme"
+              '';
+
+              subPackages = "./cmd/server";
             };
-            packages = {
-              default = pkgs.buildGoModule {
-                pname = "prisme";
-                version = "0.16.0";
-                vendorHash = "sha256-OfLvMEEp+rraejuX1B9g9Pc/pk5Xtm2ec+wxTBjSFBk=";
 
-                src = ./.;
-                # Skip go test.
-                doCheck = false;
+            docker = pkgs.dockerTools.buildImage {
+              name = "prismelabs/analytics";
+              tag = "dev";
 
-                postBuild = ''
-                  mv "$GOPATH/bin/server" "$GOPATH/bin/prisme"
-                '';
+              copyToRoot = [ pkgs.cacert ];
+              runAsRoot = ''
+                #!${pkgs.runtimeShell}
+                mkdir -p /app
+                cp -r ${
+                  self.packages."${system}".prisme-healthcheck
+                }/bin/* /healthcheck
+              '';
 
-                subPackages = "./cmd/server";
-              };
-
-              docker = pkgs.dockerTools.buildImage {
-                name = "prismelabs/analytics";
-                tag = "dev";
-
-                copyToRoot = [ pkgs.cacert ];
-                runAsRoot = ''
-                  #!${pkgs.runtimeShell}
-                  mkdir -p /app
-                  cp -r ${self.packages."${system}".prisme-healthcheck}/bin/* /healthcheck
-                '';
-
-                config = {
-                  Cmd = [ "${self.packages.${system}.default}/bin/prisme" ];
-                  WorkingDir = "/app";
-                  Env = [ "PRISME_ADMIN_HOSTPORT=0.0.0.0:9090" ];
-                };
-              };
-
-              prisme-healthcheck = pkgs.writeShellApplication {
-                name = "prisme-healthcheck";
-                runtimeInputs = with pkgs; [ wget ];
-                text = ''
-                  wget --no-verbose --tries=1 --spider "http://localhost:''${PRISME_PORT:-80}/api/v1/healthcheck" || exit 1
-                '';
+              config = {
+                Cmd = [ "${self.packages.${system}.default}/bin/prisme" ];
+                WorkingDir = "/app";
+                Env = [ "PRISME_ADMIN_HOSTPORT=0.0.0.0:9090" ];
               };
             };
-          });
-    in
-    outputsWithSystem // outputsWithoutSystem;
+
+            prisme-healthcheck = pkgs.writeShellApplication {
+              name = "prisme-healthcheck";
+              runtimeInputs = with pkgs; [ wget ];
+              text = ''
+                wget --no-verbose --tries=1 --spider "http://localhost:''${PRISME_PORT:-80}/api/v1/healthcheck" || exit 1
+              '';
+            };
+          };
+        });
+    in outputsWithSystem // outputsWithoutSystem;
 }
--- go.mod
+++ go.mod
@@ -1,25 +1,25 @@
 module github.com/prismelabs/analytics
 
-go 1.21
+go 1.22.0
 
-toolchain go1.21.5
+toolchain go1.22.3
 
 require (
-	github.com/ClickHouse/clickhouse-go/v2 v2.26.0
+	github.com/ClickHouse/clickhouse-go/v2 v2.29.0
 	github.com/cespare/xxhash/v2 v2.3.0
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/gofiber/storage v1.3.3
 	github.com/gofiber/storage/memory v1.3.4
-	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/google/uuid v1.6.0
 	github.com/google/wire v0.6.0
 	github.com/negrel/ringo v0.7.0
 	github.com/oschwald/maxminddb-golang v1.13.1
-	github.com/prometheus/client_golang v1.19.1
+	github.com/prometheus/client_golang v1.20.4
 	github.com/prometheus/client_model v0.6.1
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.9.0
-	github.com/tidwall/gjson v1.17.1
+	github.com/tidwall/gjson v1.17.3
 	github.com/ua-parser/uap-go v0.0.0-20240611065828-3a4781585db6
 )
 
@@ -29,23 +29,21 @@ require (
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/hashicorp/golang-lru v1.0.2 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
 	github.com/philhofer/fwd v1.1.3-0.20240612014219-fbbf4953d986 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/common v0.59.1 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
-	github.com/tinylib/msgp v1.2.0 // indirect
-	go.opentelemetry.io/otel v1.28.0 // indirect
-	go.opentelemetry.io/otel/trace v1.28.0 // indirect
-	golang.org/x/tools v0.23.0 // indirect
+	github.com/tinylib/msgp v1.2.1 // indirect
+	go.opentelemetry.io/otel v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
@@ -55,16 +53,16 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/compress v1.17.10 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.55.0
+	github.com/valyala/fasthttp v1.56.0
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
