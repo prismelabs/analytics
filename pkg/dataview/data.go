225c6ac43459ca0a0e22764c6ff0164c6755c452
Alexandre Negrel <alexandre@negrel.dev>
2025-07-25 10:40:11 +0200

    simplify dataview.JsonData

--- pkg/dataview/data.go
+++ pkg/dataview/data.go
@@ -10,55 +10,35 @@ var (
 	ErrMalformedData = errors.New("malformed data")
 )
 
-// dataState holds whether data is unknown, was validated and is (or not) valid.
-type dataState uint8
-
-const (
-	unknownData dataState = iota
-	invalidData
-	validData
-)
-
 // Data define a slice of byte encoded.
 type Data []byte
 
 // JsonData define data that is potentially encoded as JSON value.
 type JsonData struct {
-	data  Data
-	state dataState
+	data      Data
+	validated bool
 }
 
 // NewJsonData creates a new JsonData holding given untrusted inner data.
 func NewJsonData(data Data) *JsonData {
 	return &JsonData{
-		data:  data,
-		state: unknownData,
+		data:      data,
+		validated: false,
 	}
 }
 
 // Data returns underlying data and a nil error if it contains a well formed
 // JSON.
 func (jd *JsonData) Data() (Data, error) {
-	if jd.state == unknownData {
+	if !jd.validated {
 		if gjson.ValidBytes(jd.data) {
-			jd.state = validData
+			jd.validated = true
 		} else {
-			jd.state = invalidData
+			return nil, ErrMalformedData
 		}
 	}
 
-	switch jd.state {
-	case invalidData:
-		return nil, ErrMalformedData
-
-	case validData:
-		return jd.data, nil
-
-	case unknownData:
-		fallthrough
-	default:
-		panic("unexpected dataview.dataState")
-	}
+	return jd.data, nil
 }
 
 // JsonValidator returns nil if input is valid json and ErrMalformedData
