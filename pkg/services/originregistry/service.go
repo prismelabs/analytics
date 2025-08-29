e6522e6502fef0ceb3f5df79f17a6a3b4b70ba02
Alexandre Negrel <alexandre@negrel.dev>
2025-08-29 20:49:23 +0200

    #5: add wildcard support to origin registry

--- pkg/services/originregistry/service.go
+++ pkg/services/originregistry/service.go
@@ -15,36 +15,108 @@ type Service interface {
 }
 
 type service struct {
-	logger  log.Logger
-	origins map[string]struct{}
+	logger      log.Logger
+	origins     map[string]struct{}
+	hasWildcard bool
 }
 
 // NewService returns a new origin registry Service.
-func NewService(cfg Config, logger log.Logger) Service {
+func NewService(cfg Config, logger log.Logger) (Service, error) {
 	logger = logger.With(
 		"service", "originregistry",
 		"service_impl", "envvar",
 	)
 
+	srv := &service{logger: logger,
+		origins:     make(map[string]struct{}),
+		hasWildcard: false,
+	}
+
 	origins := make(map[string]struct{})
-	for _, src := range strings.Split(cfg.Origins, ",") {
-		origins[strings.TrimSpace(src)] = struct{}{}
+	for _, origin := range strings.Split(cfg.Origins, ",") {
+		wildcard := false
+
+		origin = strings.TrimSpace(origin)
+		if origin == "" {
+			continue
+		}
+
+		if origin[0] == '*' {
+			// *.negrel.dev become .negrel.dev
+			origin = origin[1:]
+			wildcard = true
+
+			// *.fr and *bar.foo.fr are not allowed.
+			if strings.Count(origin, ".") < 2 || !strings.HasPrefix(origin, ".") {
+				return nil, errors.New("wildcard is only allowed at subdomain level (e.g. *.negrel.dev or *.www.negrel.dev)")
+			}
+		}
+
+		// foo..fr and .fr are invalid.
+		labels := strings.Split(origin, ".")
+		if len(labels) < 2 {
+			return nil, errors.New("invalid origin")
+		}
+		for i, l := range labels {
+			if (i > 0 || !wildcard) && strings.TrimSpace(l) == "" {
+				return nil, errors.New("invalid origin")
+			}
+		}
+
+		// www.*.negrel.dev is not allowed.
+		if strings.ContainsRune(origin, '*') {
+			return nil, errors.New("wildcard is only allowed at the beginning of an origin (e.g. *.negrel.dev)")
+		}
+
+		srv.hasWildcard = srv.hasWildcard || wildcard
+		srv.origins[origin] = struct{}{}
+	}
+
+	if len(srv.origins) == 0 {
+		return nil, errors.New("no valid origin provided")
 	}
 
 	logger.Info("env var based origin registry configured", "origins", origins)
 
-	return service{logger, origins}
+	return srv, nil
 }
 
 // IsOriginRegistered implements Service.
-func (evs service) IsOriginRegistered(_ context.Context, origin string) (bool, error) {
+func (evs *service) IsOriginRegistered(_ context.Context, origin string) (bool, error) {
+	if len(origin) > 256 {
+		return false, nil
+	}
+
 	_, ok := evs.origins[origin]
+	if !ok && evs.hasWildcard {
+		// if origin is foo.bar.negrel.dev we first check
+		// if there is .negrel.dev then .bar.negrel.dev
+		// .negrel.dev should match *.negrel.dev
+		tldIdx := strings.LastIndexByte(origin, '.')
+		if tldIdx < 0 {
+			goto end
+		}
+		domainIdx := strings.LastIndexByte(origin[:tldIdx], '.')
+		if domainIdx < 0 {
+			goto end
+		}
+
+		idx := domainIdx
+		for idx != -1 {
+			_, ok = evs.origins[origin[idx:]]
+			if ok {
+				goto end
+			}
+			idx = strings.LastIndexByte(origin[:idx], '.')
+		}
+	}
+
+end:
 	evs.logger.Debug(
 		"checked if origin is registered",
 		"origin", origin,
 		"origin_registered", ok,
 	)
-
 	return ok, nil
 }
 
@@ -55,7 +127,7 @@ type Config struct {
 
 // RegisterOptions registers Config fields as options.
 func (c *Config) RegisterOptions(f *configue.Figue) {
-	f.StringVar(&c.Origins, "origins", "", "comma separated `list` of allowed origins without scheme (e.g. localhost, example.com, prismeanalytics.com)")
+	f.StringVar(&c.Origins, "origins", "", "comma separated `list` of allowed origins without scheme (e.g. localhost, *.example.com, prismeanalytics.com)")
 }
 
 // Validate validates configuration options.
--- pkg/services/originregistry/service_test.go
+++ pkg/services/originregistry/service_test.go
@@ -9,13 +9,57 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestEnvVarService(t *testing.T) {
+func TestService(t *testing.T) {
+	logger := log.New("env_var_service_test", io.Discard, false)
+
+	t.Run("NewService", func(t *testing.T) {
+		t.Run("Error", func(t *testing.T) {
+			t.Run(".fr", func(t *testing.T) {
+				service, err := NewService(Config{Origins: ".com"}, logger)
+				require.Error(t, err)
+				require.Nil(t, service)
+			})
+			t.Run("foo.fr", func(t *testing.T) {
+				service, err := NewService(Config{Origins: "foo..fr"}, logger)
+				require.Error(t, err)
+				require.Nil(t, service)
+			})
+			t.Run("**.foo.fr", func(t *testing.T) {
+				service, err := NewService(Config{Origins: "**.foo.fr"}, logger)
+				require.Error(t, err)
+				require.Nil(t, service)
+			})
+			t.Run("*foo.fr", func(t *testing.T) {
+				service, err := NewService(Config{Origins: "*foo.fr"}, logger)
+				require.Error(t, err)
+				require.Nil(t, service)
+			})
+			t.Run("bar.*.foo.fr", func(t *testing.T) {
+				service, err := NewService(Config{Origins: "*foo.fr"}, logger)
+				require.Error(t, err)
+				require.Nil(t, service)
+			})
+			t.Run("NoOrigin", func(t *testing.T) {
+				service, err := NewService(Config{Origins: ""}, logger)
+				require.Error(t, err)
+				require.Nil(t, service)
+			})
+		})
+		t.Run("Success", func(t *testing.T) {
+			service, err := NewService(Config{
+				Origins: "      example.com          , example.fr, www.negrel.dev,*.github.com         ",
+			}, logger)
+			require.NoError(t, err)
+			require.NotNil(t, service)
+		})
+	})
+
 	t.Run("IsOriginRegistered", func(t *testing.T) {
 		ctx := context.Background()
-		logger := log.New("env_var_service_test", io.Discard, false)
 
 		t.Run("NonRegistered", func(t *testing.T) {
-			service := NewService(Config{Origins: "notexample.com"}, logger)
+			service, err := NewService(Config{Origins: "notexample.com"}, logger)
+			require.NoError(t, err)
 
 			isRegistered, err := service.IsOriginRegistered(ctx, "example.com")
 			require.NoError(t, err)
@@ -23,7 +67,10 @@ func TestEnvVarService(t *testing.T) {
 		})
 
 		t.Run("Registered", func(t *testing.T) {
-			service := NewService(Config{Origins: "example.org,example.com, example.io"}, logger)
+			service, err := NewService(Config{
+				Origins: "example.org,example.com, example.io, *.example.fr",
+			}, logger)
+			require.NoError(t, err)
 
 			isRegistered, err := service.IsOriginRegistered(ctx, "example.com")
 			require.NoError(t, err)
@@ -36,6 +83,42 @@ func TestEnvVarService(t *testing.T) {
 			isRegistered, err = service.IsOriginRegistered(ctx, "example.io")
 			require.NoError(t, err)
 			require.True(t, isRegistered)
+
+			isRegistered, err = service.IsOriginRegistered(ctx, "example.fr")
+			require.NoError(t, err)
+			require.False(t, isRegistered)
+
+			isRegistered, err = service.IsOriginRegistered(ctx, "foo.bar.baz.example.fr")
+			require.NoError(t, err)
+			require.True(t, isRegistered)
 		})
 	})
+}
+
+func BenchmarkServiceNoWildcard(b *testing.B) {
+	logger := log.New("env_var_service_test", io.Discard, false)
+	service, err := NewService(Config{Origins: "example.org,example.com, example.io"}, logger)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	ctx := context.Background()
+	origins := []string{"example.com", "foo.example.com", "www.negrel.dev"}
+	for i := range b.N {
+		_, _ = service.IsOriginRegistered(ctx, origins[i%len(origins)])
+	}
+}
+
+func BenchmarkServiceWithWildcards(b *testing.B) {
+	logger := log.New("env_var_service_test", io.Discard, false)
+	service, err := NewService(Config{Origins: "example.org,example.com, *.example.io"}, logger)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	ctx := context.Background()
+	origins := []string{"example.com", "foo.bar.example.io", "www.negrel.dev"}
+	for i := range b.N {
+		_, _ = service.IsOriginRegistered(ctx, origins[i%len(origins)])
+	}
 }
